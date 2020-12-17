0c068ae48386572572652d46382fd94340c740e2
TQ <756342530@qq.com>
2020-12-18 00:58:20 +0800

    init

--- deploy.hs
+++
@@ -1,9 +0,0 @@
-yarn build &&
-git checkout gh-pages &&
-rm -rf  src/ *.html *.js *.css *.png *.jpg &&
-mv dist/* ./ &&
-rm -rf dist &&
-git add . &&
-git commit -m 'updata' &&
-git push &&
-git checkout -
