8759ad50b1cc37d8a44d833d77bb22a89d6b138f
TQ <756342530@qq.com>
2021-01-08 23:05:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# webpack 配置
+
+html-webpack-plugin
+
+css-loader
+
+style-loader
+
+webpack-dev-server
+
+sass-loader dart-sass
+
+less less-loader
+
+stylus stylus-loader
+
+file-loader
---
+++ deploy_gitee.hs
@@ -0,0 +1,9 @@
+yarn build &&
+git checkout gh-pages &&
+rm -rf  src/ *.html *.js *.css *.png *.jpg &&
+mv dist/* ./ &&
+rm -rf dist &&
+git add . &&
+git commit -m 'updata' &&
+git push gh-pages:master &&
+git checkout -
